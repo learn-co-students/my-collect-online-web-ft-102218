7d71963a252a381ddc004548969beffa6512a02a
thisisSDK <25039980+thisisSDK@users.noreply.github.com>
2018-10-29 20:13:27 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i += 1
+  end
+collect
+end
 
 
+#Why didn't I have to include this code? 
+#my_collect(array) do |name|
+#  name.split(" ").first
